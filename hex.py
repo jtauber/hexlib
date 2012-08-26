649e0ae5b3c96562ba9f83e274102764eefd7dad
James Tauber <jtauber@jtauber.com>
2012-08-25 22:16:05 -0400

    reverse direction calculation

--- hex.py
+++ hex.py
@@ -38,15 +38,31 @@ def test2():
     assert opposite(6) == 3
 
 
-def opposite(d):
-    return d + 3 if d < 4 else d - 3
+def opposite(direction):
+    return direction + 3 if direction < 4 else direction - 3
+
+
+# and so:
+
+
+def test3():
+    assert hex_in_direction([3, 5, 1], 4) == [3, 5, 0]
+    assert hex_in_direction([3, 5, 2], 5) == [3, 5, 0]
+    assert hex_in_direction([3, 5, 3], 6) == [3, 5, 0]
+    assert hex_in_direction([3, 5, 4], 1) == [3, 5, 0]
+    assert hex_in_direction([3, 5, 5], 2) == [3, 5, 0]
+    assert hex_in_direction([3, 5, 6], 3) == [3, 5, 0]
 
 
 def hex_in_direction(start, direction):
     if start[-1] == 0:
         return start[:-1] + [direction]
+    if start[-1] == opposite(direction):
+        return start[:-1] + [0]
+
 
 test1()
 test2()
+test3()
 
 print("all tests passed.")
